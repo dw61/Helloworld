b50a3faa948af35c413c0123aea4cd7736152d97
dw61 <wltok@live.com>
2021-10-29 16:33:18 -0400

    change setup.py in second branch, which is in pr

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name="hbar",
-    version="0.0.39",
+    version="0.0.40",
     author_email="xx@virginia.edu",
     description="A Project.",
     long_description="no description hahaha surprise",
