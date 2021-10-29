658a24d2c802e7d84e2b7c824343ad50d855f046
dw61 <wltok@live.com>
2021-10-29 14:29:06 -0400

    baseline

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name="hbar",
-    version="0.0.31",
+    version="0.0.32",
     author_email="xx@virginia.edu",
     description="A Project.",
     long_description="no description hahaha surprise",
