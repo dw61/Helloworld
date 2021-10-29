da4995a11e980218ebeb812ddd09afb2eafb1599
lw7jz <93140583+lw7jz@users.noreply.github.com>
2021-10-29 17:55:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name="hbar",
-    version="0.0.45",
+    version="0.0.46",
     author_email="xx@virginia.edu",
     description="A Project.",
     long_description="no description hahaha surprise",
