50f119b667a376fb4c5bd0c1eef2b5417cc3aeca
dw61 <wltok@live.com>
2021-10-29 16:35:22 -0400

    indent paths in yaml and bump setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 setuptools.setup(
     name="hbar",
-    version="0.0.40",
+    version="0.0.41",
     author_email="xx@virginia.edu",
     description="A Project.",
     long_description="no description hahaha surprise",
